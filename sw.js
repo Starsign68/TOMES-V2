2b48d314f6fe49fb153fcef60385aac89a15db85
Starsign68 <37559540+Starsign68@users.noreply.github.com>
2022-04-18 12:06:31 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,59 @@
-// Files to cache
-const cacheName = 'TOMES-V2';
-const appFiles = [
-    'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/',
-];
+var CURRENT_CACHES = {
+    data: 'data-cache-v1'
+};
 
-// Installing Service Worker
-self.addEventListener('install', (e) => {
-    alert('[Service Worker] Install');
-    e.waitUntil((async () => {
-        const cache = await caches.open(cacheName);
-        alert('[Service Worker] Caching all: app shell and content');
-        await cache.addAll(contentToCache);
-    })());
+self.addEventListener('activate', function (event) {
+    // Delete all caches that aren't named in CURRENT_CACHES.
+    var expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES));
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (!expectedCacheNamesSet.has(cacheName)) {
+                        // If this cache name isn't present in the set of "expected" cache names, then delete it.
+                        console.log('Deleting out of date cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
-// Fetching content using Service Worker
-self.addEventListener('fetch', (e) => {
-    e.respondWith((async () => {
-        const r = await caches.match(e.request);
-        alert(`[Service Worker] Fetching resource: ${e.request.url}`);
-        if (r) return r;
-        const response = await fetch(e.request);
-        const cache = await caches.open(cacheName);
-        alert(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone());
-        return response;
-    })());
+
+
+self.addEventListener('fetch', function (event) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+        caches.open(CURRENT_CACHES.data).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                if (response) {
+                    console.log(' Found response in cache:', response);
+                    return response;
+                }
+
+                console.log(' No response for %s found in cache. About to fetch ' +
+                    'from network...', event.request.url);
+
+                return fetch(event.request.clone()).then(function (response) {
+                    console.log('  Response for %s from network is: %O',
+                        event.request.url, response);
+                    if (response.status < 400 &&
+                        response.headers.has('content-type')) {
+                        console.log('  Caching the response to', event.request.url);
+                        cache.put(event.request, response.clone());
+                    } else {
+                        console.log('  Not caching the response to', event.request.url);
+                    }
+                    // Return the original response object, which will be used to fulfill the resource request.
+                    return response;
+                });
+            }).catch(function (error) {
+                console.error('Error in fetch handler:', error);
+
+                throw error;
+            });
+        })
+    );
 });
